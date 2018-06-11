5a2cae2ae1e9cc57ff70b2f93e425489f1c630d7
JJC <jjc@nlplab.cc>
2018-06-11 14:41:49 +0800

    shuffle player position every time so that the evaluation is fair

--- play.py
+++ play.py
@@ -3,20 +3,22 @@ from game import Game
 from agent import AgentRandom, AgentMedium, MyAgent
 
 import logging
+import random
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+    agents = [AgentMedium('Iron Man'), AgentMedium('Superman'),
+              MyAgent('Batman'), AgentMedium('Spiderman')]
+    scores = {agent.id: 0 for agent in agents}
 
-    scores = [0] * 4
     for _ in range(1000):
-        game = Game(
-            [AgentMedium('Iron Man'), AgentRandom('Superman'),
-             AgentRandom('Batman'), AgentRandom('Spiderman')]
-        )
+        random.shuffle(agents)
+        game = Game(agents)
 
         game.set_game()
+
         for i, score in enumerate(game.play()):
-            scores[i] += score
+            scores[agents[i].id] += score
 
     print(scores)
